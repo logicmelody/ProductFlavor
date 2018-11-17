5f5787b107160354de3d7db8163e436aa1acb1eb
Danny Lin <logicmelody@gmail.com>
2018-11-17 20:35:01 +0800

    Setup gj, yc product flavors

--- app/src/main/java/com/dl/productflavor/MainActivity.java
+++ app/src/main/java/com/dl/productflavor/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.title)
+    @BindView(R.id.text_view_main_app_name)
     public TextView mTextViewAppName;
 
     @Override
@@ -18,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialize() {
+        setupAppName();
+    }
 
+    private void setupAppName() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BuildConfig.PRODUCT)
+                .append("\n")
+                .append(this.getPackageName());
+
+        mTextViewAppName.setText(sb.toString());
     }
 }
